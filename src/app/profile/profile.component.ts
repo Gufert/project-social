d6a16c9ce3e435e7bd2b3a59c91b5578ddda5fec
Gufert <82119484+Gufert@users.noreply.github.com>
2023-02-07 12:49:50 -0600

    changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import Profile from '../../testing/profile.json';
+
+interface Profile {
+  name: String;
+  username: String;
+  pfp: String;
+  bio: String;
+  location: String,
+  link: String,
+  joinDate: String,
+  followingCount: Number,
+  followerCount: Number
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +19,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  profile: Profile = Profile;
 }
