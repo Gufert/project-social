958367f695688d57ae5b7d4fa07a54f4021c4b9f
Zachary Shand <ztshand@gmail.com>
2023-02-24 12:53:41 -0600

    creating environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC7HFZL6UIsi1cZHZkMS-KAtHSQLt-qJew",
+        authDomain: "project-social-923a2.firebaseapp.com",
+        databaseURL: "https://project-social-923a2-default-rtdb.firebaseio.com",
+        projectId: "project-social-923a2",
+        storageBucket: "project-social-923a2.appspot.com",   
+        messagingSenderId: "823960909578",
+        appId: "1:823960909578:web:5c77455415470d525d2cf3",
+        measurementId: "G-4ZB73RC827"
+    }
+  };
