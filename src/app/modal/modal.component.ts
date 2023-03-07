129fc29e43c27698a03ce12183e9733abf633f6b
Gufert <82119484+Gufert@users.noreply.github.com>
2023-03-07 08:50:49 -0600

    removed thing

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,12 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ModalService } from '../modal.service';
 
 @Component({
     selector: 'app-modal',
-    templateUrl: 'modal.component.html',
-    styleUrls: ['modal.component.css'],
-    encapsulation: ViewEncapsulation.None
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
     constructor(protected modalService: ModalService) { }
