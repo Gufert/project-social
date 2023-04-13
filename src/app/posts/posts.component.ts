b035e22d4112f825d6c3e523a989e52871717b08
Zachary Shand <ztshand@gmail.com>
2023-04-13 17:38:08 -0500

    Basic like/dislike functionality

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { UserData } from '../shared/services/user-data';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { GetUserService } from '../shared/services/get-user.service';
+import { LikeDislike } from '../shared/services/like-dislike';
+import { LikeDislikeService } from '../shared/services/like-dislike.service';
+import { User } from '../shared/services/user';
 
 @Component({
   selector: 'app-posts',
@@ -12,7 +15,18 @@ import { GetUserService } from '../shared/services/get-user.service';
 export class PostsComponent implements OnInit{
   user: UserData = {} as UserData
   @Input() post?: any;
-  constructor(public router: Router, public afs: AngularFirestore, public getUserService: GetUserService) { }
+
+  constructor(
+    public router: Router,
+    public afs: AngularFirestore,
+    public getUserService: GetUserService,
+    public likeDislikeService: LikeDislikeService
+    ) {
+    this.currentUser = JSON.parse(localStorage.getItem('user')!);
+  }
+
+  ld: LikeDislike = new LikeDislike();
+  currentUser: User;
 
   async ngOnInit() {
     this.post.date = new Date(this.post.date.seconds * 1000);
@@ -29,6 +43,25 @@ export class PostsComponent implements OnInit{
 
   postClick(event: any, click: String){
     event.stopPropagation();
+    if(click == 'like'){
+      this.ld = {
+        uid: this.currentUser.uid,
+        pid: '',
+        date: new Date()
+      }
+      this.likeDislikeService.likePost(this.ld)
+    }
+    if(click == 'dislike'){
+      this.ld = {
+        uid: this.currentUser.uid,
+        pid: '',
+        date: new Date()
+      }
+      this.likeDislikeService.dislikePost(this.ld)
+    }
+    if(click == 'reply'){
+
+    }
     console.log(click);
   }
 }
