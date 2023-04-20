12b2c0e85497421212e82daa1fbe6a8e0cb3be46
Gufert <82119484+Gufert@users.noreply.github.com>
2023-04-19 20:17:09 -0500

    bookmarks

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from '../shared/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Post } from '../shared/services/post';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.css']
 })
-export class BookmarksComponent {
 
+export class BookmarksComponent {
+  bookmarks: any[] = [];
+  posts: Post[] =[];
+
+  constructor(public authService: AuthService, public afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.getBookmarks();
+      }
+    });
+  }
+
+  getBookmarks(){
+    this.afs.collection("bookmarks",ref=>ref.where("uid", "==", this.authService.userData.uid).orderBy("date","desc")).get()
+    .subscribe((data) => {
+      data.forEach((el) => {
+        this.bookmarks.push(el.data());
+      })
+      this.bookmarks.forEach((bookmark) => {
+        this.afs.collection("posts").doc(bookmark.pid).ref.get().then((doc) => {
+          this.posts.push(<Post>doc.data());
+        })
+      })
+    });
+    
+  }
 }
--- src/app/shared/services/interactions.service.ts
+++ src/app/shared/services/interactions.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from './auth.service';
+import { deleteDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InteractionsService {
 
-  constructor() { }
+  constructor(public authService: AuthService, public afs: AngularFirestore) { }
 
   like(){
 
@@ -15,8 +18,31 @@ export class InteractionsService {
 
   }
 
-  bookmark(){
-
+  async bookmark(pid: string){
+    this.afs.collection("bookmarks",ref=>ref.where("uid","==",this.authService.userData.uid).where("pid","==",pid)).get()
+    .subscribe((data) => {
+      if(data.size > 0){
+        data.forEach((doc) => {
+          var docData: any = doc.data();
+          this.afs.collection("bookmarks").doc(docData.bid).delete();
+        })
+        return false;
+      }
+      else{
+        return this.afs.collection("bookmarks").add({
+          date: new Date(),
+          uid: this.authService.userData.uid,
+          pid: pid
+        }).then((docRef) =>{
+          let newDocID = docRef.id
+          docRef.set({
+            bid: newDocID},
+            {merge: true});
+        }).catch((error) =>{
+          console.error("Error", error)
+        })
+      }
+    });
   }
 
   share(){
