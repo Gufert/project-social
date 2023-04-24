4e6230f4500a2783b152bd67c8ee7c3f35e0e5ff
Gufert <82119484+Gufert@users.noreply.github.com>
2023-04-24 18:08:13 -0500

    test

--- src/app/shared/services/auth.service.spec.ts
+++ src/app/shared/services/auth.service.spec.ts
@@ -1,4 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { ModalService } from './modal.service';
+import { ToastrService } from 'ngx-toastr';
 
 import { AuthService } from './auth.service';
 
@@ -6,7 +14,16 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AngularFireAuth,
+        AngularFirestore,
+        AngularFirestoreDocument,
+        Router,
+        ModalService,
+        ToastrService
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
