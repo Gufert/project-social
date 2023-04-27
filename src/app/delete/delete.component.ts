79cc7058ff450bab064a318b259c36121e1e0480
Shandy-Man <103293699+Shandy-Man@users.noreply.github.com>
2023-04-27 17:23:39 -0500

    generate component

---
+++ src/app/delete/delete.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PostService } from '../shared/services/post.service';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { GetUserService } from '../shared/services/get-user.service';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteComponent implements OnInit {
+
+  @Input() pid = ""
+  
+  constructor(
+    public authService: AuthService,
+    public postService: PostService,
+    public router: Router,
+    public afs: AngularFirestore,
+    public getUserService: GetUserService,
+  ) {
+
+  }
+  
+  ngOnInit() {
+    
+  }
+
+  deletePost(){
+    
+  }
+}
