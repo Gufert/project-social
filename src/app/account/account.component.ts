b85d1be8e9b81f202ed1b857676915992e2f184a
Gufert <82119484+Gufert@users.noreply.github.com>
2023-05-04 10:53:22 -0500

    update username on page

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/services/auth.service';
 import { GetUserService } from '../shared/services/get-user.service';
 import { UserData } from '../shared/services/user-data';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from 'firebase/auth';
 
 @Component({
@@ -9,12 +10,14 @@ import { User } from 'firebase/auth';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit{
+export class AccountComponent {
   userData: UserData = {} as UserData;
 
-  constructor(public authService: AuthService, public getUserService: GetUserService) {}
-
-  async ngOnInit() {
-    this.userData = await this.getUserService.UserFromUID(this.authService.userData.uid);
+  constructor(public authService: AuthService, public getUserService: GetUserService, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(async (user) => {
+      if(user){
+        this.userData = await this.getUserService.UserFromUID(user.uid);
+      }
+    })
   }
 }
